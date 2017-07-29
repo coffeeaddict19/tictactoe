6eb7f1a1472eb0641886c43700349a5bfda869d1
John Laird <coffeeaddict19@comcast.net>
2017-07-29 13:16:44 -0500

    first draft
    First working draft of TicTacToe game. Functional but needs cleanup particularly regarding AI.

---
+++ tictactoe_bottle.py
@@ -0,0 +1,57 @@
+from bottle import default_app, post, get, request, response, template
+import tictactoe_logic
+import json
+import uuid
+
+session_dict = {}
+
+@get('/tictactoe')
+def tictactoe_get_handler():
+    return template('tictactoe_page.html')
+
+@post('/tictactoe')
+def tictactoe_post_handler():
+    jsonoutput = None
+    try:
+        # parse input data
+        try:
+            data = request.json
+        except:
+            print 'threw exception on parse'
+            raise ValueError
+
+        if data is None:
+            print 'threw exception on no data recieved'
+            raise ValueError
+            
+        print 'data', data
+        if "requestType" in data:
+            typeOfRequest = data["requestType"]
+            if typeOfRequest == "newgame":
+                sessionid = str(uuid.uuid4())
+                session_dict[sessionid] = {'logic':tictactoe_logic.tictactoeGame()}
+                jsonoutput = {'session_id':sessionid}
+            elif typeOfRequest == "playgame":
+                if "play" in data and "session_id" in data:
+                    playin = data['play']
+                    sessionid_in = data['session_id']
+                    logic = session_dict[sessionid_in]['logic']
+                    playout = logic.makeplay(playin)
+                    gamestate_out = playout[0]
+                    boardstate_out = playout[1]
+                    jsonoutput = {'gameboard_grid_x': boardstate_out[0], 'gameboard_grid_o': boardstate_out[1], 'gameresult_end': gamestate_out}
+                else:
+                    raise ValueError
+            else:
+                raise ValueError
+
+    except ValueError:
+        # if bad request data, return 400 Bad Request
+        response.status = 400
+        return
+    
+    # return 200 Success
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps(jsonoutput)
+    
+application = default_app()
---
+++ tictactoe_logic.py
@@ -0,0 +1,147 @@
+import random
+
+class tictactoeGame:
+    def __init__(self):
+        self.gamestate = 0 #0=in play;1=win;2=tie
+        self.player1_moves = set()
+        self.player2_moves = set()
+        self.current_player_turn = 0 #0=player1;1=player2
+        print 'init'
+    
+    def validatemove(self, move):
+        print 'validating move', move
+        #is the 'move' in range?
+        if not self.gamestate == 0:
+            print 'game is not in play'
+            return False
+        if move > 8 or move < 0:
+            print 'move is not in range'
+            return False
+        if move in self.player1_moves | self.player2_moves:
+            print 'play already made'
+            return False
+        print 'move', move, 'evaluated as legal'
+        return True
+        
+    def getgamestate(self):
+        if self.gamestate == 0:
+            return "in play"
+        elif self.gamestate == 1:
+            return "win"
+        else:
+            return "tie"
+            
+    def getcurrentplayerturn(self):
+        if self.current_player_turn == 0:
+            return "player 1"
+        elif self.current_player_turn == 1:
+            return "player 2"
+    
+    def getopenmoves(self):
+        allmoves = {0,1,2,3,4,5,6,7,8}
+        return allmoves-(self.player1_moves | self.player2_moves)
+        
+    def playhumanmove(self, move):
+        if self.validatemove(move) == True and self.current_player_turn == 0:
+            print 'adding move', move, 'to player player', self.player1_moves
+            self.player1_moves.add(move)
+            self.current_player_turn = 1
+            print 'human play made at', move
+            return True
+        else:
+            print 'human play made at', move, 'was rejected'
+            return False
+    
+    def evaluateWinState(self,movesetToEval):
+        #012
+        #345
+        #678
+        winStateList = [{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}]
+        for winState in winStateList:
+            if winState <= movesetToEval:
+                return True
+        return False
+        
+    def evaluateWinStateNext(self,movesetToEval):
+        openmoves = self.getopenmoves()
+        for move in openmoves:
+            movesetToEval_wMove = movesetToEval.copy()
+            movesetToEval_wMove.add(move)
+            if self.evaluateWinState(movesetToEval_wMove):
+                return move
+        return None
+    
+    def playaimove(self):
+        player1move_count = len(self.player1_moves)
+        moveToMake = None
+        #ai's first turn (human has made one move)
+        if player1move_count == 1:
+            #if human played to center, play a corner
+            if 4 in self.player1_moves:
+                moveToMake = random.choice((0,2,6,8))
+            #if human not played in center, play in the center
+            elif 4 not in self.player1_moves:
+                moveToMake = 4
+            else:
+                raise AssertionError("unexpected ui state")
+        #ai's second turn (human has made two moves)
+        elif player1move_count == 2:
+            #we can't win but we can prevent a win by player 1
+            moveToMake = self.evaluateWinStateNext(self.player1_moves)
+            #if the human player can win by making a move, let ai make it to block
+            #if theh human player cannot win by making a move, then make a random move
+            if moveToMake == None:
+                moveToMake = random.choice(tuple(self.getopenmoves()))
+        #ai's third turn going forward (human has made 3 moves)
+        else:
+            #can we win by making a play?
+            moveToMake = self.evaluateWinStateNext(self.player2_moves)
+            #can we prevent the human from winning by making a play?
+            if moveToMake == None:
+                moveToMake = self.evaluateWinStateNext(self.player1_moves)
+            #make a random move
+            if moveToMake == None:
+                moveToMake = random.choice(tuple(self.getopenmoves()))
+        print 'adding move', moveToMake, 'to player ai', self.player2_moves
+        self.player2_moves.add(moveToMake)
+        self.current_player_turn = 0
+        print 'ai play made at', moveToMake
+                
+    def getBoardOut(self):
+        return [list(self.player1_moves), list(self.player2_moves)]
+    
+    def makeplay(self, play):
+        play1win = self.evaluateWinState(self.player1_moves)
+        play2win = self.evaluateWinState(self.player2_moves)
+        nomovesleft = len(self.getopenmoves()) == 0
+        if play1win == True:
+            self.gamestate = 1 #win
+            return ['human won',self.getBoardOut()]
+        elif play2win == True:
+            self.gamestate = 1 #win
+            return ['ai won',self.getBoardOut()]
+        elif nomovesleft == True:
+            self.gamestate = 2 #tie
+            return ['tie',self.getBoardOut()]
+        else:
+            #human player 1's turn
+            #prompt
+            self.playhumanmove(int(play))
+            nomovesleft = len(self.getopenmoves()) == 0
+            if nomovesleft == False:
+                self.playaimove()
+            play1win = self.evaluateWinState(self.player1_moves)
+            play2win = self.evaluateWinState(self.player2_moves)
+            nomovesleft = len(self.getopenmoves()) == 0
+            if play1win == True:
+                self.gamestate = 1 #win
+                return ['human won',self.getBoardOut()]
+            elif play2win == True:
+                self.gamestate = 1 #win
+                return ['ai won',self.getBoardOut()]
+            elif nomovesleft == True:
+                self.gamestate = 2 #tie
+                return ['tie',self.getBoardOut()]
+            else:
+                self.gamestate = 0 #still in play
+                return ['in play',self.getBoardOut()]
